a376b0477e9e2e5577f060485e6563bcd3945180
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2024-07-17 13:40:17 -0700

    add ca cert.

---
+++ dev/api/app/config/config.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+
+export default {
+  development: {
+    username: 'postgres',
+    password: 'postgrespw',
+    database: 'postgres-mqJP',
+    host: '127.0.0.1',
+    port: '49153',
+    dialect: 'postgres',
+    seedStorage: 'sequelize',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seedStorage: 'sequelize',
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync('../files/ca.pem').toString(),
+    },
+  },
+  staging: {
+    use_env_variable: 'DATABASE_URL',
+  },
+};
